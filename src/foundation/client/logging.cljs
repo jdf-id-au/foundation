5e80476ea305753760521918b7de1148f8249450
jdf-id-au <jeremy.field@gmail.com>
2020-04-20 17:56:00 +1000

    client tick adaptor, logging

---
+++ src/foundation/client/logging.cljs
@@ -0,0 +1,18 @@
+(ns foundation.client.logging
+  "Direct use of Javascript Console logging.
+   ClojureScript data structures render nicely in Chrome DevTools thanks to `binaryage/devtools`.")
+
+(def debug js/console.debug)
+(def info js/console.info)
+(def warn js/console.warn)
+(def error js/console.error)
+(defn show [v] (debug v) v)
+
+(defn table
+  ([x] (js/console.table
+         (if (or (array? x) (object? x))
+           x
+           (clj->js x))))
+  ([x & xs] (table (cons x xs))))
+
+; TODO group ... groupEnd
